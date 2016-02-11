8c2d4b67d6125fe774655f89dddd55c7542b8265
fmm8 <fmm8@njit.edu>
2016-02-11 18:59:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,24 +4,31 @@ echo "My page";
   
 ?>
 
+<!-- Below goes html -->
+
   <!DOCTYPE html>
   <html lang = "en">
   <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <style>
-      body{background-color: red;
-          font-size: 20px;
-          font-family: sans serif;
-          color: #ffffff;
-      }
-      
-      p{
-        font-weight:light;
-      }
+
     </style>
   </head>
   <body>
   
-    <p>hello world</p>
-  
+  <div class="container-fluid">
+        <h1>My First Bootstrap Page</h1>
+        <p>This is some text.</p> 
+  </div>
+    
+    
+    
+    
+    
+    
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   </body>
   </html>
