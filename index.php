adabeaf8017709c2664a090a3024fd2f6acbd811
fmm8 <fmm8@njit.edu>
2016-02-12 13:52:09 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,18 +2,6 @@
 
 <!-- Below goes html -->
 
-  <!DOCTYPE html>
-  <html lang = "en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="bootstrap-theme.min.css">
-    <style>
-
-    </style>
-  </head>
-  <body>
   
 <!DOCTYPE html>
 <html lang="en">
@@ -24,6 +12,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <link rel="stylesheet" href="custom_theme.css">
+  <link rel="stylesheet" href="bootstrap-theme.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
