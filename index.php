c950cc65e657b987885b89293ecc170e94703318
fmm8 <fmm8@njit.edu>
2016-02-12 14:50:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -90,44 +90,44 @@
 <!-- Start of Contact container -->
 
 <div class="container-fluid bg-4 text-center" id="contact">    
-<section>
-<h3>Contact Me</h3>
-<div class="row">
-<div class="col-sm-4">
-fmm8@njit.edu
-New Jersey
-</div>
-<div class="col-sm-8">
-<form class="form-horizontal" role="form">
-<div class="form-group">
-<label class="control-label col-sm-2" for="email">Email:</label>
-<div class="col-sm-10">
-<input type="email" class="form-control" id="email" placeholder="Enter email">
-</div>
-</div>
-<div class="form-group">
-<label class="control-label col-sm-2" for="pwd">Password:</label>
-<div class="col-sm-10">          
-<input type="password" class="form-control" id="pwd" placeholder="Enter password">
-</div>
-</div>
-<div class="form-group">        
-<div class="col-sm-offset-2 col-sm-10">
-<div class="checkbox">
-<label><input type="checkbox"> Remember me</label>
-</div>
-</div>
-</div>
-<div class="form-group">        
-<div class="col-sm-offset-2 col-sm-10">
-<button type="submit" class="btn btn-default">Submit</button>
-</div>
-</div>
-</form>
+  <section>
+    <h3>Contact Me</h3>
+    <div class="row">
+      <div class="col-sm-4">
+        <p>fmm8@njit.edu</p>
+        <p>New Jersey</p>
+      </div>
+      <div class="col-sm-8">
+        <form class="form-horizontal" role="form">
+          <div class="form-group">
+            <label class="control-label col-sm-2" for="email">Email:</label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" id="email" placeholder="Enter email">
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="control-label col-sm-2" for="pwd">Password:</label>
+          <div class="col-sm-10">          
+            <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+          </div>
+        </div>
+        <div class="form-group">        
+          <div class="col-sm-offset-2 col-sm-10">
+            <div class="checkbox">
+              <label><input type="checkbox"> Remember me</label>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">        
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default">Submit</button>
+          </div>
+        </div>
+        </form>
 
-</div>
-</div>
-</section>
+      </div><!-- end of sm-8 -->
+    </div><!-- end of row group -->
+  </section>
 </div><!-- End of Contact Container -->
 
     
