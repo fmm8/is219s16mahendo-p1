53eac8ccc004458d29f52df5a722481d25aab84f
fmm8 <fmm8@njit.edu>
2016-02-12 13:34:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="index_min_bs.css">
+    <link rel="stylesheet" href="bootstrap-theme.min.css">
     <style>
 
     </style>
