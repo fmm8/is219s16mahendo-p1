9ac260054ad12f1786f9dd74bbcc3fccc9e0a3c2
fmm8 <fmm8@njit.edu>
2016-02-12 14:30:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -32,9 +32,9 @@
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
         <li><a href="#education">Education</a></li>
-        <li><a href="#interests">Interests</a></li>
-        <li><a href="#workExperience">Work Experience</a></li>
-        <li><a href="#contact">Contact Me</a></li>
+        <li><a href="#">Interests</a></li>
+        <li><a href="#">Work Experience</a></li>
+        <li><a href="#">Contact Me</a></li>
       </ul>
     </div>
   </div>
@@ -74,7 +74,7 @@
 <!-- Start of work experience container -->
 <div class="container-fluid bg-3 text-center" id="workExperience">    
   <section>
-     <h3>Work Experience</h3>
+     <h3>Interests</h3>
      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -90,17 +90,44 @@
 <!-- Start of Contact container -->
 
 <div class="container-fluid bg-4 text-center" id="contact">    
-  <section>
-     <h3>Contact Me</h3>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-  </section>
+<section>
+<h3>Contact Me</h3>
+<div class="row">
+<div class="col-sm-4">
+fmm8@njit.edu
+New Jersey
+</div>
+<div class="col-sm-8">
+<form class="form-horizontal" role="form">
+<div class="form-group">
+<label class="control-label col-sm-2" for="email">Email:</label>
+<div class="col-sm-10">
+<input type="email" class="form-control" id="email" placeholder="Enter email">
+</div>
+</div>
+<div class="form-group">
+<label class="control-label col-sm-2" for="pwd">Password:</label>
+<div class="col-sm-10">          
+<input type="password" class="form-control" id="pwd" placeholder="Enter password">
+</div>
+</div>
+<div class="form-group">        
+<div class="col-sm-offset-2 col-sm-10">
+<div class="checkbox">
+<label><input type="checkbox"> Remember me</label>
+</div>
+</div>
+</div>
+<div class="form-group">        
+<div class="col-sm-offset-2 col-sm-10">
+<button type="submit" class="btn btn-default">Submit</button>
+</div>
+</div>
+</form>
+
+</div>
+</div>
+</section>
 </div><!-- End of Contact Container -->
 
     
