18d1084d27ff72002be12a45428481724e7e2e42
fmm8 <fmm8@njit.edu>
2016-02-11 19:06:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ echo "My page";
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="index_min_bs.css">
     <style>
 
     </style>
